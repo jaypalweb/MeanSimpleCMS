b4bfa49f45ac1b64ee5f2fd1ba00f8b505881f4a
Jay Pal <contactjaypal@gmail.com>
2020-04-26 02:00:19 +0530

    add page - add CKEditor

--- angularcms/src/app/components/admin-add-page/admin-add-page.component.ts
+++ angularcms/src/app/components/admin-add-page/admin-add-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PageService } from 'src/app/services/page.service';
+import { Router } from '@angular/router';
+
+//declare var $: any; --way to add jquery or any third party library
+declare var CKEDITOR: any;
 
 @Component({
   selector: 'app-admin-add-page',
@@ -14,15 +18,22 @@ export class AdminAddPageComponent implements OnInit {
   public content: string;
 
   constructor(
+    private router: Router,
     private pageService: PageService
   ) { }
 
   ngOnInit() {
+    if (localStorage.getItem("user") !== "\"admin\"") {
+      this.router.navigateByUrl('');
+    }
+    CKEDITOR.replace('content');
   }
 
   addPage({ form, value, valid }) {
     form.reset();
     if (valid) {
+      value.content = CKEDITOR.instances.content.getData();
+
       this.pageService.postAddPage(value).subscribe(res => {
         if (res == 'pageExists') {
           this.errorMsg = true;
@@ -34,6 +45,9 @@ export class AdminAddPageComponent implements OnInit {
           setTimeout(function () {
             this.successMsg = false;
           }.bind(this), 2000);
+
+          CKEDITOR.instances.content.setData('');
+
           this.pageService.getPages().subscribe(pages => {
             this.pageService.pagesBS.next(pages);
           });
