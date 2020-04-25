f68e3a3578c48e4608f8daad563f6e95e08b82a2
Jay Pal <contactjaypal@gmail.com>
2020-04-25 22:05:38 +0530

    bugfix

--- angularcms/src/app/app.component.ts
+++ angularcms/src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  get front() {
+export class AppComponent implements OnInit {
+  front: boolean = true;
+  //below showing some error so changed the code
+  // get front() {
+  //   if (localStorage.getItem("user") === "\"admin\"") {
+  //     return false;
+  //   }
+  //   return true;
+  // }
+  ngDoCheck() {
     if (localStorage.getItem("user") === "\"admin\"") {
-      return false;
+      this.front = false;
+    } else {
+      this.front = true;
     }
-    return true;
+  }
+  ngOnInit() {
+    if (localStorage.getItem("user") === "\"admin\"") {
+      this.front = false;
+    }
   }
 }
