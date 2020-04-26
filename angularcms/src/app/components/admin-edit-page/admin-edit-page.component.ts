7fb9d2f9bdb01689e9e60f5c60092e067339c9f3
Jay Pal <contactjaypal@gmail.com>
2020-04-26 17:59:14 +0530

    edit page

--- angularcms/src/app/components/admin-edit-page/admin-edit-page.component.ts
+++ angularcms/src/app/components/admin-edit-page/admin-edit-page.component.ts
@@ -16,6 +16,8 @@ export class AdminEditPageComponent implements OnInit {
   content: string;
   id: string;
   successMsg: boolean = false;
+  errorMsg: boolean = false;
+  errorMsg2: boolean = false;
   param: any;
 
   constructor(
@@ -37,7 +39,7 @@ export class AdminEditPageComponent implements OnInit {
         this.page = page;
         this.title = page['title'];
         this.content = page['content'];
-        this.id = page['id'];
+        this.id = page['_id'];
       });
 
       //content sometimes not comming - below a fix
@@ -46,6 +48,36 @@ export class AdminEditPageComponent implements OnInit {
       }.bind(this), 500);
 
     });
+  }
+
+  editPage({ value, valid }) {
+    if (valid) {
+      value.content = CKEDITOR.instances.content.getData();
+      this.pageService.postEditPage(value).subscribe(res => {
+        if (res == 'pageExists') {
+          this.errorMsg = true;
+          setTimeout(function () {
+            this.errorMsg = false;
+          }.bind(this), 2000);
+        } else if (res == 'problem') {
+          this.errorMsg2 = true;
+          setTimeout(function () {
+            this.errorMsg2 = false;
+          }.bind(this), 2000);
+        } else {
+          this.successMsg = true;
+          setTimeout(function () {
+            this.successMsg = false;
+          }.bind(this), 2000);
+
+          this.pageService.getPages().subscribe(pages => {
+            this.pageService.pagesBS.next(pages);
+          });
+        }
+      });
+    } else {
+      console.log('Form is not valid.');
+    }
   }
 
 }
--- nodecmsapi/routes/pages.js
+++ nodecmsapi/routes/pages.js
@@ -70,36 +70,43 @@ router.get('/edit-page/:id', function (req, res) {
     })
 });
 
-/**
- * POST edit page
- */
+/*
+* POST edit page
+*/
 router.post('/edit-page/:id', function (req, res) {
+
     var id = req.params.id;
+
     var title = req.body.title;
     var slug = req.body.title.replace(/\s+/g, '-').toLowerCase();
     var content = req.body.content;
+    var hasSidebar = req.body.hasSidebar;
+    var sidebar = (hasSidebar) ? "yes" : "no";
 
-    Page.findById(id, function (err, page) {
-        if (err)
-            console.log(err);
-
-        if (page.slug === slug) {
+    Page.findOne({ slug: slug, _id: { '$ne': id } }, function (e, p) {
+        if (e) console.log(e);
+        if (p) {
             res.json("pageExists");
         } else {
-            page.title = title;
-            page.slug = slug;
-            page.content = content;
+            Page.findById(id, function (err, page) {
+                if (err) console.log(err);
 
-            page.save(function (err) {
-                if (err) {
-                    console.log(err);
-                    res.json("error");
-                } else {
-                    res.json("ok");
-                }
+                page.title = title;
+                page.slug = slug;
+                page.content = content;
+                page.sidebar = sidebar;
+
+                page.save(function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.json("problem");
+                    } else {
+                        res.json("ok");
+                    }
+                });
             });
         }
-    })
+    });
 });
 
 //Exports
